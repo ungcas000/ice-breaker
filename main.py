1ba71aac9b8e7aa939914c4c42628efad3c0e234
gEvent <gevent@CSSI2015-2.local>
2015-07-27 17:01:24 -0400

    added feature to input times and redirects to a form that randomizes activity

--- main.py
+++ main.py
@@ -18,8 +18,12 @@ import jinja2
 import os
 import webapp2
 from google.appengine.api import users
+import random
+from random import choice
+import logging
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
 
 
 class MainHandler(webapp2.RequestHandler):
@@ -28,18 +32,40 @@ class MainHandler(webapp2.RequestHandler):
 
         self.response.write(template.render())
 
-    def post(self):
-        template = jinja_environment.get_template('templates/study.html')
-
-        self.response.write(tempalte.render())
 
 class TimerTestHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/timer.html')
         self.response.write(template.render())
 
+class StudyHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/study.html')
+        self.response.write(template.render())
+
+    def post(self):
+
+        #returns length of break and challenge
+        def getActivity():
+            activity_dict = ['Go for a run', 'Do Yoga', 'Attend a dance class']
+            activity2_dict = ['Jumping Jacks', 'Push-ups', 'Plank']
+
+            if self.request.get('break') >= '20':
+                return random.choice(activity_dict)
+
+            else:
+                return random.choice(activity2_dict)
+
+        activity = getActivity()
+
+        self.response.write('<h1> Break the Ice </h1>' + '<br>' + 'You have ' + self.request.get('break') + ' minute(s).' + '<br>')
+        self.response.write(' <h2> Your challenge: </h2> ' + activity)
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/timer', TimerTestHandler),
+    ('/study', StudyHandler),
 ], debug=True)
